63d8cb27d45f190a5942329e25fa7086d6946a7f
KyleDinges <Kyleedinges@live.com>
2020-07-10 18:25:45 -0500

    Chapter 6 update

--- pypi_org/views/package_views.py
+++ pypi_org/views/package_views.py
@@ -7,7 +7,11 @@ blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 
 @blueprint.route('/project/<package_name>')
-#@response(template_file='packages/details.html')
+# @response(template_file='packages/details.html')
 def package_details(package_name: str):
     return "Package details for {}".format(package_name)
-    # return flask.render_template('home/index.html', packages=test_packages)
+
+
+@blueprint.route('/<int:rank>')
+def popular(rank: int):
+    return "The details for  the {}th most popular package".format(rank)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pypi_org',
+    packages=['pypi_org'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
